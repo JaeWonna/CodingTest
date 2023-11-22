fa6c93c78ee279af9dcba4703d5bdab139277ea3
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-11-22 12:43:31 +0900

    Update 8-7. 바닥 공사.py

--- 이코테/Review 2/Chapter08. 다이나믹 프로그래밍/8-7. 바닥 공사.py
+++ 이코테/Review 2/Chapter08. 다이나믹 프로그래밍/8-7. 바닥 공사.py
@@ -1,11 +1,9 @@
 n = int(input())
-data = list(map(int, input().split()))
-d = [0]*n
+d = [0]*(n+1)
+d[1] = 1
+d[2] = 3
 
-d[0] = data[0]
-d[1] = max(data[0], data[1])
+for i in range(3,n+1):
+    d[i] = (d[i-1] + 2*d[i-2]) % 796796
 
-for i in range(2,n):
-    d[i] = max(data[i] + d[i-2], d[i-1])
-
-print(d[n-1])
+print(d[n])
