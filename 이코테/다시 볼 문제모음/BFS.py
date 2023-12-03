a6bfbb3fbefdac2c9e147fb36c820bd9bf497312
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-12-03 18:00:14 +0900

    Create BFS.py

---
+++ 이코테/다시 볼 문제모음/BFS.py
@@ -0,0 +1,33 @@
+# BFS -> 3개 (디큐) // 1~4단계
+
+from collections import deque
+
+n,m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+def bfs(x,y):
+    q = deque()
+    q.append((x,y))
+
+    while q:
+        x,y = q.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx,ny))
+    return graph[n-1][m-1]
+
+print(bfs(0,0))
