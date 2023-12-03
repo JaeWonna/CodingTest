de3a25bd098404da8058e005e3e6cbc8230a7896
JWJ <101786715+JaeWonna@users.noreply.github.com>
2023-12-03 17:59:51 +0900

    Create 다익스트라.py

---
+++ 이코테/다시 볼 문제모음/다익스트라.py
@@ -0,0 +1,40 @@
+# 개선된 다익스트라 복습 2번째 -> 3개 (힙큐)
+
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+v,e = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(v+1)]
+distance = [INF] *(v+1)
+
+for i in range(e):
+    a,b,cost = map(int, input().split())
+    graph[a].append((b,cost))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0,start))
+    distance[start] = 0
+
+    while q:
+        dist,now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1,v+1):
+    if distance[i] == INF:
+        print('infinity')
+    else:
+        print(distance[i])
